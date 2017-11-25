40a94625263a585d6b5a6643dd896500bddd03f7
trunghoai <trunghoai29041995@gmail.com>
2017-11-25 10:58:34 +0700

    b5

---
+++ Chat-Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+namespace Chat_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            byte[] data = new byte[1024];
+            string stringData;
+            int recv; sock.Connect(iep);
+            Console.WriteLine("Connected to server");
+            recv = sock.Receive(data); stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine("Received: {0}", stringData);
+            while (true)
+            { stringData = Console.ReadLine();
+                if (stringData == "exit") break; data = Encoding.ASCII.GetBytes(stringData);
+                sock.Send(data, data.Length, SocketFlags.None);
+                data = new byte[1024]; recv = sock.Receive(data); stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine("Received: {0}", stringData); }
+            sock.Close();
+        }
+    }
+}
